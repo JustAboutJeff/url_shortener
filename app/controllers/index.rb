eb7d545194e8516f0d4b4fd5dbdb339dd5ed8fea
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 14:29:55 -0500

    Incorporates partials

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,38 +1,23 @@
 get '/' do
-  # let user create new short URL, display a list of shortened URLs
-  @url_list = Url.all
-  @valid ||= true
   erb :index
 end
 
-# before do
-# end
-
-# error do
-# end
-
 post '/urls' do
-  @new_url = Url.new({long_url: params[:long_url], click_count: 0})
-
+  @new_url = Url.create(params)
   if @new_url.valid?
-    @valid = true
-    @new_url.save
+    @errors = nil
     @url_list = Url.all
     erb :index
   else
-    @valid = false
+    @errors = @new_url.errors.messages
     erb :index
   end
 
 end
 
-# e.g., /q6bda
 get '/:short_url' do
   @clicked_url = Url.find_by_short_url(params[:short_url])
-  puts @clicked_url
   @clicked_url.click_count += 1
   @clicked_url.save
-  puts @clicked_url.click_count
   redirect "#{@clicked_url.long_url}"
-  # redirect to appropriate "long" URL
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,15 @@
 class Url < ActiveRecord::Base
   before_save :get_short_url
-  validates :long_url, presence: true
+  before_save :get_click_count
+  validates :long_url, presence: true,
+    format: { with: /http:\/\/.*|https:\/\/.*/}
 
   def get_short_url
-    self.short_url = SecureRandom.hex(5)
+    self.short_url = SecureRandom.hex(3)
   end
+
+  def get_click_count
+    self.click_count ||= 0
+  end
+
 end
